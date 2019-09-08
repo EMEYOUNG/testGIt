ddcbf59d9e56e72860b1a1cd53c78887bea1e4d4
Young <Young@qq.com>
2019-09-08 13:35:29 +0800

    改成测试一下2.0

--- test01/src/com/mytest/helloGit.java
+++ test01/src/com/mytest/helloGit.java
@@ -3,5 +3,6 @@ package com.mytest;
 public class helloGit {
     public static void main(String[] args){
         System.out.println("就是这么的强大，没办法啊！！！");
+        System.out.println("我本来就是1.0版本啊，这次修改成1.1版本");
     }
 }
